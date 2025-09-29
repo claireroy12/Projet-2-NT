403abec48648dcf3642b3bdae8d51470174bb5a6
claireroy12 <160762347+claireroy12@users.noreply.github.com>
2025-09-29 10:53:20 -0400

    Continuation du scraper reddit | exportation en .csv
    - Ajout de subreddit afin d'avoir assez d'entrées.
    - exportation des informations dans le fichier posts.csv

--- reddit.py
+++ reddit.py
@@ -1,6 +1,7 @@
 import praw
 import datetime
 from datetime import datetime
+import pandas as pd
 
 reddit = praw.Reddit(
 client_id="rsaZdvLrhQqsjDPpPKX6AA",
@@ -11,12 +12,13 @@ user_agent="StardewScraping (by u/Either-Location-4670)"
 
 
 posts = []
+
+print(" Récupération des posts de quebec libre...")
 for post in reddit.subreddit("QuebecLibre").top(limit=2500): # récupérer 3000 posts pour être sûr
     posts.append({
     "id": post.id,
     "titre": post.title,
     "auteur": str(post.author) if post.author else None,
-    "subreddit": str(post.subreddit),
     "flair": post.link_flair_text,
     "score": int(post.score),
     "upvote_ratio": float(post.upvote_ratio),
@@ -26,10 +28,222 @@ for post in reddit.subreddit("QuebecLibre").top(limit=2500): # récupérer 3000 
     "url": post.url,
     "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
     })
-    print(post.title + " - " + str(post.score))
+
+print(" Récupération des posts de canada...")
+for post in reddit.subreddit("canada").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
+
+print(" Récupération des posts de ask a canadian...")
+for post in reddit.subreddit("AskACanadian").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
+
+print(" Récupération des posts de personnal finance canada...")
+for post in reddit.subreddit("PersonalFinanceCanada").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
+
+print(" Récupération des posts de quebec city...")
+for post in reddit.subreddit("Quebeccity").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
+
+print(" Récupération des posts de brossard...")
+for post in reddit.subreddit("brossard").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
+
+print(" Récupération des posts de canada politics...")
+for post in reddit.subreddit("CanadaPolitics").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
+
+
+
+
+print(" Récupération des posts de quebec...")
+for post in reddit.subreddit("Quebec").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
+
+print(" Récupération des posts de quebec finance...")
+for post in reddit.subreddit("QuebecFinance").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
+
+print(" Récupération des posts de montreal...")
+for post in reddit.subreddit("Montreal").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
+
+
+print(" Récupération des posts de sherbrooke...")
+for post in reddit.subreddit("Sherbrooke").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
+
+print(" Récupération des posts de trois-rivières...")
+for post in reddit.subreddit("troisrivieres").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
+
+print(" Récupération des posts de rimouski...")
+for post in reddit.subreddit("Rimouski").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
+
+print(" Récupération des posts de Laval...")
+for post in reddit.subreddit("Laval").top(limit=2500): # récupérer 3000 posts pour être sûr
+    posts.append({
+    "id": post.id,
+    "titre": post.title,
+    "auteur": str(post.author) if post.author else None,
+    "flair": post.link_flair_text,
+    "score": int(post.score),
+    "upvote_ratio": float(post.upvote_ratio),
+    "nb_commentaires": int(post.num_comments),
+    "texte": post.selftext,
+    "date_creation": datetime.fromtimestamp(post.created_utc).isoformat(),
+    "url": post.url,
+    "image_url": post.url if post.url.endswith((".jpg", ".png")) else None
+    })
 
 
 
 
 # Export CSV
-#pd.DataFrame(posts).to_csv("../data/posts.csv", index=False, encoding="utf-8")
+pd.DataFrame(posts).to_csv("Projet-2-NT/posts.csv", index=False, encoding="utf-8-sig", sep=";")
+print("Exporté posts.csv! :)")
